f2c00e0f260f50e24e4e992ceca78fd5586cd344
uncountablehollows <100173972+UncountableHollows@users.noreply.github.com>
2022-06-22 00:10:32 +1200

    First draft of build_table, currently has index out of bounds error.

--- src/main.rs
+++ src/main.rs
@@ -18,31 +18,45 @@ fn main() {
 
         let table = build_table(String::from(search_string));
         
+        for i in table {
+            print!("{} ", i);
+        }
     }
 }
 
 
 fn build_table(input: String) -> Vec<i32>{
 
-    let mut table = vec![0; 5];
+    let chars : Vec<char> = input.chars().collect();
+    let mut m = chars.len();
+
+    let mut table:[i32;5] = [0; 5];
     table[0] = -1;
     table[0] = 0;
 
-    let chars : Vec<char> = input.chars().collect();
-    let mut m = chars.len();
 
     let mut j = 0;
     let mut pos = 2;
 
     while(pos < m) {
-
+        if(chars[pos-1] == chars[j]) {
+            j += 1;
+            table[pos] = j as i32;
+            pos += 1;
+            
+        } else if(j > 0) {
+            j = table[j] as usize; //not safe from overflows, should fix that at some point.
+        } else {
+            table[pos] = 0;
+            pos += 1;
+        }
         
     }
 
+    let mut return_vec = Vec::new();
+    for n in table {
+        return_vec.push(n);
+    }
 
-
-
-
-
-    table
+    return_vec
 }
